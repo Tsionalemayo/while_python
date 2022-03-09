61b9262c6766b73156e0415e84f9c7038a2637e2
ציונה אלמיו <tsionalemayo73@gmail.com>
2022-03-09 20:40:22 +0200

    while exercises

---
+++ while exercises.py
@@ -0,0 +1,31 @@
+#1
+# x=20
+# while 0<x :
+#     if(x%2==1):
+#         print(x)
+#     x=x-1
+
+
+#3.1
+
+# x=int(input("enter positive number"))
+#
+# while x>0 :
+#     if x%2==1:
+#         print(x)
+#     x=x-1
+
+# #4
+#
+# x=int(input("enter positive number: "))
+# y=int(input("to see odd numbers Tap 1 and even numbers  Tap 2: "))
+#
+# while x>0:
+#     if y==1 and x%2==1:
+#         print(x)
+#     elif (y==2 and x%2==0):
+#         print(x)
+#     x=x-1
+# else:
+#     print("error")
+
