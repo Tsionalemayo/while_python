990e487db203369586e7ad3602397d9c851c02c1
ציונה אלמיו <tsionalemayo73@gmail.com>
2022-03-10 17:56:51 +0200

    while exercises part 2

---
+++ while exercises part 2.py
@@ -0,0 +1,11 @@
+#16
+# x=input("enter number")
+# sum=0
+# count=0
+# while x!="q":
+#         sum=sum+(int(x))
+#         count=count+1
+#         x = input("enter number")
+#
+# else:
+#     print(sum//count)
